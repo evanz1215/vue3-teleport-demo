eee57223a0b1f55d3f27b40fe9c404cdc75ae469
YEJIN\Evan <evan@metallurgietech.com>
2023-12-08 10:16:53 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+const isDisabled = ref(true);
+</script>
+
+<template>
+  <div>
+    <!-- TODO:Teleport -->
+    <button @click="isDisabled = !isDisabled">移動</button>
+    <teleport to=".box" :disabled="isDisabled">
+      <p>移動我到 box</p>
+    </teleport>
+  </div>
+</template>
+
+<style scoped></style>
